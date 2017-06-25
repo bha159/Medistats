04314ccf79bdc93d1243894f08bd10bc67cf6c04
Bharat Kumar <bharat.159753@gmail.com>
2017-06-26 02:20:43 +0530

    Splash screen added for different screen sizes

--- app/src/main/java/com/example/android/medistats/Splash.java
+++ app/src/main/java/com/example/android/medistats/Splash.java
@@ -1,42 +1,22 @@
 package com.example.android.medistats;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by ratcoder on 23/6/17.
  */
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.Menu;
-import android.view.WindowManager;
+public class Splash extends AppCompatActivity
+{
 
-public class Splash extends Activity {
-
-    /**
-     * Duration of wait
-     **/
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
-
-    /**
-     * Called when the activity is first created.
-     */
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.splash);
-
-        /* New Handler to start the Menu-Activity
-         * and close this Splash-Screen after some seconds.*/
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(Splash.this, Menu.class);
-                Splash.this.startActivity(mainIntent);
-                Splash.this.finish();
-            }
-        }, SPLASH_DISPLAY_LENGTH);
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
