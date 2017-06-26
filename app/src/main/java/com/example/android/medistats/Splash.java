b950f8de56531d9087259fef4de063e8aae0fcb7
Bharat Kumar <bharat.159753@gmail.com>
2017-06-26 13:32:30 +0530

    Main screen added

--- app/src/main/java/com/example/android/medistats/Splash.java
+++ app/src/main/java/com/example/android/medistats/Splash.java
@@ -1,6 +1,12 @@
 package com.example.android.medistats;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -18,5 +24,21 @@ public class Splash extends AppCompatActivity
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public BitmapDrawable writeOnDrawable(int drawableId, String text)
+    {
+
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), drawableId).copy(Bitmap.Config.ARGB_8888, true);
+
+        Paint paint = new Paint();
+        //paint.setStyle(Style.FILL);
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(20);
+
+        Canvas canvas = new Canvas(bm);
+        canvas.drawText(text, 0, bm.getHeight()/2, paint);
+
+        return new BitmapDrawable(bm);
     }
 }
