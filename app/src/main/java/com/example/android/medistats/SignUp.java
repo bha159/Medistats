6f71fe294784376738e81883b65b4ec6a9cbb893
Bharat Kumar <bharat.159753@gmail.com>
2017-07-18 03:13:24 +0530

    Sign up created for user and doctor

--- app/src/main/java/com/example/android/medistats/SignUp.java
+++ app/src/main/java/com/example/android/medistats/SignUp.java
@@ -1,12 +1,15 @@
 package com.example.android.medistats;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.app.ProgressDialog;
@@ -35,9 +38,61 @@ public class SignUp extends AppCompatActivity
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        TextView user_login;
+        TextView doc_login;
+        ImageButton user;
+        ImageButton doc;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
         ButterKnife.inject(this);
+        final LinearLayout doc_up = (LinearLayout)findViewById(R.id.doc_up);
+        final LinearLayout user_up = (LinearLayout)findViewById(R.id.user_up);
+        user = (ImageButton)findViewById(R.id.user);
+        doc = (ImageButton)findViewById(R.id.user);
+        user_login = (TextView)findViewById(R.id.user_login);
+        doc_login  = (TextView)findViewById(R.id.doc_login);
+
+        user.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                user_up.setVisibility(View.VISIBLE);
+                //doc_up.setVisiblity(View.GONE);
+            }
+        });
+
+        doc.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                user_up.setVisibility(View.GONE);
+                //doc_up.setVisiblity(View.VISIBLE);
+            }
+        });
+
+        user_login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(SignUp.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        doc_login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(SignUp.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
        /* _signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -135,4 +190,18 @@ public class SignUp extends AppCompatActivity
 
         return valid;
     }*/
+
+    @Override
+    public void onBackPressed()
+    {
+        Intent intent = new Intent(SignUp.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+    }
 }
