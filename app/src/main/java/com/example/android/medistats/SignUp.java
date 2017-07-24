b04f205e70137318de1c28d777f2e224f2190594
Bharat Kumar <bharat.159753@gmail.com>
2017-07-24 03:31:01 +0000

    New changes and graphics added

--- app/src/main/java/com/example/android/medistats/SignUp.java
+++ app/src/main/java/com/example/android/medistats/SignUp.java
@@ -1,209 +1,105 @@
 package com.example.android.medistats;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.app.ProgressDialog;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import butterknife.ButterKnife;
-import butterknife.InjectView;
 
-public class SignUp extends AppCompatActivity
-{
-    private static final String TAG = "SignupActivity";
 
-    /*@InjectView(R.id.doc_name) EditText docname;
-    @InjectView(R.id.hospital_name) EditText hospital_name;
-    @InjectView(R.id.doc_email) EditText doc_email;
-    @InjectView(R.id.input_password) EditText _passwordText;
-    @InjectView(R.id.btn_signup) Button _signupButton;
-    @InjectView(R.id.link_login) TextView _loginLink;*/
+public class SignUp extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         TextView user_login;
         TextView doc_login;
-        ImageButton user;
-        ImageButton doc;
+        ImageButton user,doc;
+        Button upload;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
-        ButterKnife.inject(this);
-        final LinearLayout doc_up = (LinearLayout)findViewById(R.id.doc_up);
-        final LinearLayout user_up = (LinearLayout)findViewById(R.id.user_up);
-        user = (ImageButton)findViewById(R.id.user);
-        doc = (ImageButton)findViewById(R.id.doc);
-        user_login = (TextView)findViewById(R.id.user_login);
-        doc_login  = (TextView)findViewById(R.id.doc_login);
+        final LinearLayout doc_up = (LinearLayout) findViewById(R.id.doc_up);
+        final LinearLayout user_up = (LinearLayout) findViewById(R.id.user_up);
+        user = (ImageButton) findViewById(R.id.user);
+        doc = (ImageButton) findViewById(R.id.doc);
+        user_login = (TextView) findViewById(R.id.user_login);
+        doc_login = (TextView) findViewById(R.id.doc_login);
+        upload = (Button) findViewById(R.id.doc_proof);
 
         user.setOnClickListener(new View.OnClickListener()
         {
-
             @Override
-            public void onClick(View arg0)
+            public void onClick(View button)
             {
-                user_up.setVisibility(View.VISIBLE);
-                doc_up.setVisibility(View.GONE);
-            }
+                    //Set the button's appearance
+                    button.setSelected(!button.isSelected());
+                    if (button.isSelected())
+                    {
+                        user_up.setVisibility(View.VISIBLE);
+                        doc_up.setVisibility(View.GONE);
+                        //Handle selected state change
+                    }
+                }
 
         });
 
         doc.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick(View arg0)
+            public void onClick(View button)
             {
-                user_up.setVisibility(View.GONE);
-                doc_up.setVisibility(View.VISIBLE);
+                //Set the button's appearance
+                button.setSelected(!button.isSelected());
+                if (button.isSelected())
+                {
+                    user_up.setVisibility(View.GONE);
+                    doc_up.setVisibility(View.VISIBLE);
+                    //Handle selected state change
+                }
             }
+
         });
 
-        user_login.setOnClickListener(new View.OnClickListener()
-        {
+        user_login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(SignUp.this,MainActivity.class);
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
 
-        doc_login.setOnClickListener(new View.OnClickListener()
-        {
+        doc_login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(SignUp.this,MainActivity.class);
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
 
-       /* _signupButton.setOnClickListener(new View.OnClickListener() {
+        upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signup();
+                Intent intent = new Intent(SignUp.this, CameraPreview.class);
+                startActivity(intent);
+                finish();
             }
         });
 
-        _loginLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Finish the registration screen and return to the Login activity
-                finish();
-            }
-        });*/
     }
-
-    /*public void signup()
-    {
-        Log.d(TAG, "Signup");
-
-        if (!validate()) {
-            onSignupFailed();
-            return;
-        }
-
-        _signupButton.setEnabled(false);
-
-        final ProgressDialog progressDialog = new ProgressDialog(SignUpDoc.this,R.style.AppTheme_Dark_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Creating Account...");
-        progressDialog.show();
-
-        String name = _nameText.getText().toString();
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-        // TODO: Implement your own signup logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onSignupSuccess or onSignupFailed
-                        // depending on success
-                        onSignupSuccess();
-                        // onSignupFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
-    }
-
-
-    public void onSignupSuccess()
-    {
-        _signupButton.setEnabled(true);
-        setResult(RESULT_OK, null);
-        finish();
-    }
-
-    public void onSignupFailed()
-    {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
-        _signupButton.setEnabled(true);
-    }
-
-    public boolean validate()
-    {
-        boolean valid = true;
-
-        String name = _nameText.getText().toString();
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-        if (name.isEmpty() || name.length() < 3) {
-            _nameText.setError("at least 3 characters");
-            valid = false;
-        } else {
-            _nameText.setError(null);
-        }
-
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
-            valid = false;
-        } else {
-            _emailText.setError(null);
-        }
-
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
-            valid = false;
-        } else {
-            _passwordText.setError(null);
-        }
-
-        return valid;
-    }*/
 
     @Override
-    public void onBackPressed()
-    {
-        Intent intent = new Intent(SignUp.this,MainActivity.class);
+    public void onBackPressed() {
+        Intent intent = new Intent(SignUp.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
 
     @Override
-    protected void onDestroy()
-    {
+    protected void onDestroy() {
         super.onDestroy();
     }
 }
