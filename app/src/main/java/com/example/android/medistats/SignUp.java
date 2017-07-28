7367963b82cc3cc4e335ab9e3931a0e525fe8542
Bharat Kumar <bharat.159753@gmail.com>
2017-07-28 22:58:45 +0000

    User home screen updated

--- app/src/main/java/com/example/android/medistats/SignUp.java
+++ app/src/main/java/com/example/android/medistats/SignUp.java
@@ -49,7 +49,7 @@ public class SignUp extends AppCompatActivity
                 user_up.setVisibility(View.VISIBLE);
                 doc_up.setVisibility(View.GONE);
                 user.setImageResource(R.drawable.user1);
-                doc.setImageResource(R.drawable.doc1);
+                doc.setImageResource(R.drawable.doc);
             }
         });
 
@@ -61,7 +61,7 @@ public class SignUp extends AppCompatActivity
                 user_up.setVisibility(View.GONE);
                 doc_up.setVisibility(View.VISIBLE);
                 user.setImageResource(R.drawable.user);
-                doc.setImageResource(R.drawable.doc);
+                doc.setImageResource(R.drawable.doc1);
             }
         });
 
